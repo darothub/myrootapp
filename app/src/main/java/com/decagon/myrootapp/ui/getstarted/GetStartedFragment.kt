2ab933d01d00fd9181d89d4a49d49970ad4a4e4b
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-03-08 13:00:35 +0100

    finished the get started ui

--- app/src/main/java/com/decagon/myrootapp/ui/getstarted/GetStartedFragment.kt
+++ app/src/main/java/com/decagon/myrootapp/ui/getstarted/GetStartedFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 
 import com.decagon.myrootapp.R
 
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -35,27 +36,8 @@ class GetStartedFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_started, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_get_started, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GetStartedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GetStartedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return view
     }
 }
