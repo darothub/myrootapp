f4e5ada1b180131ed84dad1a06f10014e3fc8b7a
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-03-08 13:39:09 +0100

    added the navigation of the fragments

--- app/src/main/java/com/decagon/myrootapp/ui/getstarted/GetStartedFragment.kt
+++ app/src/main/java/com/decagon/myrootapp/ui/getstarted/GetStartedFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.get
 
 import com.decagon.myrootapp.R
+import kotlinx.android.synthetic.main.fragment_get_started.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -39,5 +42,16 @@ class GetStartedFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_get_started, container, false)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        get_started_login_btn.setOnClickListener {
+            findNavController().navigate(R.id.action_getStartedFragment_to_loginFragment)
+        }
+
+        get_started_signup_btn.setOnClickListener {
+            findNavController().navigate(R.id.action_getStartedFragment_to_signUpFragment)
+        }
     }
 }
