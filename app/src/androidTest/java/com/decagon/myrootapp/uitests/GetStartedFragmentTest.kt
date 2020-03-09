d1c72d53cca6794babad5a14ebc2f56b09896bf6
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-03-09 01:17:52 +0100

    tested ui and navigation

---
+++ app/src/androidTest/java/com/decagon/myrootapp/uitests/GetStartedFragmentTest.kt
@@ -0,0 +1,71 @@
+package com.decagon.myrootapp.uitests
+
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.decagon.myrootapp.R
+import com.decagon.myrootapp.ui.getstarted.GetStartedFragment
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.security.AccessController
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class GetStartedFragmentTest {
+
+//    @get:Rule
+//    val fragmentrule  =
+    private lateinit var navController: TestNavHostController
+    private lateinit var factory:FragmentFactory
+
+    @Before
+    fun setup(){
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        navController.setGraph(R.navigation.welcome_nav)
+        factory = FragmentFactory()
+    }
+
+    @Test
+    fun test_signUpButton_displays(){
+        val getStartedFramentScenario = launchFragmentInContainer<GetStartedFragment>()
+        onView(withId(R.id.get_started_signup_btn)).check(matches(withText("Signup")))
+    }
+
+    @Test
+    fun test_navigation_login(){
+        val getStartedFramentScenario = launchFragmentInContainer<GetStartedFragment>()
+
+        getStartedFramentScenario.onFragment {
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        onView(withId(R.id.get_started_login_btn)).perform(ViewActions.click())
+        assertThat(navController.currentDestination?.id, `is`(R.id.loginFragment))
+    }
+
+    @Test
+    fun test_navigation_signup(){
+        val getStartedFramentScenario = launchFragmentInContainer<GetStartedFragment>()
+
+        getStartedFramentScenario.onFragment {
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        onView(withId(R.id.get_started_signup_btn)).perform(ViewActions.click())
+        assertThat(navController.currentDestination?.id, `is`(R.id.signUpFragment))
+    }
+}
