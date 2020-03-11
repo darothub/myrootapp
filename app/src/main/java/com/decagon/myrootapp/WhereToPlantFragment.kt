434905912a0626dbc12b1ef357e3edd2f07c35f6
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2020-03-11 12:32:06 +0100

    imported assets

---
+++ app/src/main/java/com/decagon/myrootapp/WhereToPlantFragment.kt
@@ -0,0 +1,22 @@
+package com.decagon.myrootapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class WhereToPlantFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_where_to_plant, container, false)
+    }
+
+}
