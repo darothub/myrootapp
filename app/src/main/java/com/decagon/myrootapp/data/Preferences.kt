3796f9b68f642c04aa67654002fcbfe456b23494
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2020-03-17 01:53:14 +0100

    saved email to preferences

--- app/src/main/java/com/decagon/myrootapp/data/Preferences.kt
+++ app/src/main/java/com/decagon/myrootapp/data/Preferences.kt
@@ -7,6 +7,7 @@ class Preferences {
     companion object{
         private const val PREF_NAME = "user"
         private const val USER_AUTH = "token"
+        private const val USER_MAIL = "email"
 
         fun saveAuthToken(activity: Activity, token: String){
             val preference = activity.getPreferences(Context.MODE_PRIVATE)
@@ -21,5 +22,17 @@ class Preferences {
             return preferences.getString(USER_AUTH, "").toString()
         }
 
+        fun setEmail(activity: Activity, email: String){
+            val preference = activity.getPreferences(Context.MODE_PRIVATE)
+            with (preference.edit()) {
+                putString(USER_MAIL, email)
+                commit()
+            }
+        }
+
+        fun getEmail(activity: Activity): String{
+            val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString(USER_MAIL, "").toString()
+        }
     }
 }
