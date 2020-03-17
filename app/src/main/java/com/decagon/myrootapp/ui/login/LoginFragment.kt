078c3272b93a169c1201fe19b4c7083cc31b80d8
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2020-03-17 03:18:19 +0100

    add input verification

--- app/src/main/java/com/decagon/myrootapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/decagon/myrootapp/ui/login/LoginFragment.kt
@@ -3,6 +3,8 @@ package com.decagon.myrootapp.ui.login
 import android.graphics.Paint
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 
 import com.decagon.myrootapp.R
+import com.decagon.myrootapp.data.models.login.LoginBody
+import com.decagon.myrootapp.data.models.login.LoginResponse
+import com.decagon.myrootapp.utils.InputUtils
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
@@ -52,6 +57,44 @@ class LoginFragment : Fragment() {
         login_btn.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_dashboardActivity)
         }
+
+        login_password_input.editText!!.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString().length < 6){
+                    login_password_input.editText!!.error = "Password must 6 characters or more"
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+
+    }
+
+    private fun login(): LoginResponse{
+        var loginResponse: LoginResponse? = null
+        when{
+            login_email_input.editText!!.text.toString().isEmpty() ->{
+                login_email_input.editText!!.error = "Email cannot be empty"
+            }
+            InputUtils.validateEmail(login_email_input.editText!!.text.toString())->{
+                login_email_input.editText!!.error = "Email not valid"
+            }
+            login_password_input.editText!!.text.toString().isEmpty() -> {
+                login_password_input.editText!!.error = " Password cannot be empty"
+            }
+            else ->{
+                val email = login_email_input.editText!!.text.toString()
+                val password = login_password_input.editText!!.text.toString()
+                val loginBody = LoginBody(email, password)
+                loginResponse = viewModel.login(loginBody)
+            }
+        }
+        return loginResponse!!
     }
 
 }
