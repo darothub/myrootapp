1da6636cb68406d42f650f30d3a1de7de89864de
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2020-03-17 02:01:56 +0100

    saved id and phone to preferences

--- app/src/main/java/com/decagon/myrootapp/data/Preferences.kt
+++ app/src/main/java/com/decagon/myrootapp/data/Preferences.kt
@@ -8,6 +8,9 @@ class Preferences {
         private const val PREF_NAME = "user"
         private const val USER_AUTH = "token"
         private const val USER_MAIL = "email"
+        private const val USER_NAME = "name"
+        private const val USER_ID = "id"
+        private const val USER_PHONE = "phone"
 
         fun saveAuthToken(activity: Activity, token: String){
             val preference = activity.getPreferences(Context.MODE_PRIVATE)
@@ -33,6 +36,45 @@ class Preferences {
         fun getEmail(activity: Activity): String{
             val preferences = activity.getPreferences(Context.MODE_PRIVATE)
             return preferences.getString(USER_MAIL, "").toString()
+        }
+
+        fun saveName(activity: Activity, name: String){
+            val preference = activity.getPreferences(Context.MODE_PRIVATE)
+            with (preference.edit()) {
+                putString(USER_NAME, name)
+                commit()
+            }
+        }
+
+        fun getName(activity: Activity): String {
+            val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString(USER_NAME, "").toString()
+        }
+
+        fun saveID(activity: Activity, id: String){
+            val preference = activity.getPreferences(Context.MODE_PRIVATE)
+            with (preference.edit()) {
+                putString(USER_ID, id)
+                commit()
+            }
+        }
+
+        fun getID(activity: Activity):String{
+            val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString(USER_ID, "").toString()
+        }
+
+        fun savePhone(activity: Activity, phone: String){
+            val preference = activity.getPreferences(Context.MODE_PRIVATE)
+            with (preference.edit()) {
+                putString(USER_PHONE, phone)
+                commit()
+            }
+        }
+
+        fun getPhone(activity: Activity): String{
+            val preferences = activity.getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString(USER_PHONE, "").toString()
         }
     }
 }
